0294d7870e974acf22b36a3bbc459353707ff1be
Eduardo Rigon <edukatiau@gmail.com>
2023-12-07 19:48:17 -0300

    verifica se aluno tem emprestimo pendente

--- src/br/com/projetobiblioteca/apresentacao/telamenu/alunos/TelaRemoverAluno.java
+++ src/br/com/projetobiblioteca/apresentacao/telamenu/alunos/TelaRemoverAluno.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import br.com.projetobiblioteca.model.Aluno;
 import br.com.projetobiblioteca.model.Funcionário;
 import br.com.projetobiblioteca.persistencia.AlunoDAO;
+import br.com.projetobiblioteca.persistencia.EmprestimoDAO;
 import br.com.projetobiblioteca.utils.Colors;
 
 public class TelaRemoverAluno {
@@ -24,6 +25,11 @@ public class TelaRemoverAluno {
         if (aluno == null) {
             System.out.println(Colors.ANSI_RED + "Aluno não encontrado!" + Colors.ANSI_RESET);
         } else {
+
+            if(verificarEmprestimo()){
+                return;
+            }
+
             System.out.println(aluno.toString());
             System.out.println("Confirma a remoção? (S/N)");
             String confirmacao = sc.next().toUpperCase();
@@ -36,6 +42,17 @@ public class TelaRemoverAluno {
             }
             System.out.println(Colors.ANSI_BLUE + "-------------------" + Colors.ANSI_RESET);
         }
+    }
+
+    public static boolean verificarEmprestimo(){
+        EmprestimoDAO emprestimoDAO = new EmprestimoDAO();
+
+        if(emprestimoDAO.buscarTodos().size() > 0){
+            System.out.println(Colors.ANSI_RED + "Não é possível remover um aluno com empréstimos pendentes!" + Colors.ANSI_RESET);
+            return true;
+        }
+        
+        return false;
     }
 
 }
